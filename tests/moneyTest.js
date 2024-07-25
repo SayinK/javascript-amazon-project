8a20a164f821a722fd72652991ffd1ab1a5d9266
SayinK <emily082505@gmail.com>
2024-07-25 17:53:41 -0500

    Created automated tests for formatCurrency()

---
+++ tests/moneyTest.js
@@ -0,0 +1,29 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+console.log('test suite: format currency')
+
+console.log('converts cents into dollars')
+formatCurrency(2095);
+if(formatCurrency(2095) === '20.95'){
+    console.log('passed');
+}
+else{
+    console.log('failed');
+};
+console.log('works with 0')
+if (formatCurrency(0) === '0.00'){
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
+console.log('rounds up to the nearest cent')
+if(formatCurrency(2000.5)==='20.01'){
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
+
+//basic test case: to see if code is working
+//edge case: test values that are a bit more tricky
