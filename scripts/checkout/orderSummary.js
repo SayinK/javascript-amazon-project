e5008a906fccc6311a3cc1a99eff6c363047ee86
SayinK <emily082505@gmail.com>
2024-07-25 17:36:33 -0500

    The order summary resets after a product is deleted

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,12 +4,7 @@ import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js'; //this is called named export
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'; //this is called a default export
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-hello();
-
-const today = dayjs();
-const deliveryDate = today.add(7,'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import {renderPaymentSummary} from './paymentSummary.js';
 
 export function renderOrderSummary(){
     let cartSummaryHTML = '';
@@ -114,6 +109,8 @@ export function renderOrderSummary(){
 
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         container.remove();
+
+        renderPaymentSummary();
     });
     });
 
@@ -125,8 +122,6 @@ export function renderOrderSummary(){
         })
     })
 }
-
-renderOrderSummary();  
 
 //MVC
 //1. Model: saves and manages the data
